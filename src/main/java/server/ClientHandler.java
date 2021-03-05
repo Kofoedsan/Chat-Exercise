34803b7f1500cda3c3660b3162ccfce4fcbfa3a2
Thomas Overgaard <68824543+Schwencke@users.noreply.github.com>
2021-03-05 23:37:12 +0100

    basic poolfunctions and connection to client

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,4 +1,36 @@
 package server;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable{
+
+    Socket clientSocket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    public ClientHandler(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            out.writeUTF("Hello from thread: "+Thread.currentThread().getName());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        try {
+            out.writeUTF("Connected");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        //TODO: Remember to close streams
+    }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,29 +1,73 @@
 package server;
 
-import java.net.InetAddress;
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.UnknownHostException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server {
+public class Server implements Runnable{
+
+    protected ServerSocket serverSocket = null;
+    protected int serverPort;
+    protected Socket clientSocket = null;
+    protected Thread runningThread= null;
+    protected boolean isStopped    = false;
+
+    protected ExecutorService threadPool =
+            Executors.newFixedThreadPool(10);
+
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (this){
+            this.runningThread = Thread.currentThread();
+        }
+        openSS();
+        while(!isStopped()){
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            try {
+                this.threadPool.execute(new ClientHandler(clientSocket));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
 
 
-    //Call server with arguments like this: 0.0.0.0 8088 logfile.log
-    public static void main(String[] args) throws UnknownHostException {
-        String ip ="localhost";
-        int port = 8088;
-        String logFile = "log.txt";  //Do we need this
+
+
+    }
+
+    public static void main(String[]args){
+        Server server = new Server(8080);
+        new Thread(server).start();
+       //TODO: Make the server stop on command server.stop();
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+    public synchronized void stop(){
+        this.isStopped = true;
         try {
-            if (args.length == 3) {
-                ip = args[0];
-                port = Integer.parseInt(args[1]);
-                logFile = args[2];
-            }
-            else {
-                throw new IllegalArgumentException("Server not provided with the right arguments");
-            }
-        } catch (NumberFormatException ne) {
-            System.out.println("Illegal inputs provided when starting the server!");
-            return;
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+    private void openSS(){
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException ioException) {
+            throw new RuntimeException("Error opening port : " + serverPort, ioException);
         }
     }
 }
