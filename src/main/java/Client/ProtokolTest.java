71c1284d48ca08d7b77dfaff02e3ab04ec5269be
Thorbjørn Jensen <cph-tj308@cphbusiness.dk>
2021-03-10 09:14:36 +0100

    added ProtokolTest

---
+++ src/main/java/Client/ProtokolTest.java
@@ -0,0 +1,14 @@
+package Client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProtokolTest {
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
